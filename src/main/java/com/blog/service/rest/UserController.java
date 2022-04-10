2ff304a7aefa79bf82e80e3f49af68102c7b4f84
pawankumargithub <pkgupta789428@gmail.com>
2022-04-10 11:39:08 +0530

    logger added to controller

--- src/main/java/com/blog/service/rest/UserController.java
+++ src/main/java/com/blog/service/rest/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +19,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blog.service.demo.service.UserService;
+import com.blog.service.demo.serviceimpl.UserserviceImpl;
 import com.blog.service.dto.UserDTO;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 	@Autowired
 	private UserService service;
 	
 	
 	@PostMapping("/create")
 	public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO dto){
+		LOGGER.info("inside user controller:"+dto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.createUser(dto));
 	}
 	
